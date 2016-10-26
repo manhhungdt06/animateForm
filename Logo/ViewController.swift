3b84a6cd70844c3c53fd1b93590ce70ff89bc212
manhhung.dt6 <manhhung.dt6@gmail.com>
2016-10-26 14:19:21 +0700

    update...

--- Logo/ViewController.swift
+++ Logo/ViewController.swift
@@ -12,20 +12,28 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var logo_img: UIImageView!
     @IBOutlet weak var lbl_text: UILabel!
+    @IBOutlet weak var lbl_name: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         logo_img!.alpha = 0
         lbl_text!.alpha = 0
+        lbl_name!.alpha = 0
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        UIView.animate(withDuration: 5, animations: {
+        UIView.animate(withDuration: 3, animations: {
             self.logo_img!.alpha = 1
             }, completion: { (finished) in
-                UIView.animate(withDuration: 5, animations: {
+                UIView.animate(withDuration: 3, animations: {
                     self.lbl_text!.center = CGPoint(x: self.logo_img!.center.x, y: 400)
                     self.lbl_text!.alpha = 1
+                }, completion: { (finished) in
+                        UIView.animate(withDuration: 5, animations: {
+                            self.lbl_name!.center = CGPoint(x: self.logo_img!.center.x, y: 600)
+                            // self.lbl_text!.alpha = 0
+                            self.lbl_name!.alpha = 1
                 })
+            })
         })
     }
 }
